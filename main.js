8a483bd87a725ca6f7ca6091c27c292bb4db0101
alexander <oviedojalexander@gmail.com>
2024-11-22 13:03:22 -0300

    practica1

---
+++ main.js
@@ -0,0 +1,280 @@
+// var textoa = "ALEXANDER";
+// var textob = "OVIEDO";
+
+// console.log(textoa + "                      " + textob);
+
+// var texto1 = "hola mañana paso por tu casa despues de verte";
+// var texto2 = "salimos a comprar un poco de helado";
+// var texto3 = "vemos peli de terrror";
+
+// console.log(texto1 + " " + texto2 + " " + texto3);
+
+// var numero1 = 25;
+// var numero2 = 25;a t
+// // var numero3 = 50;
+// // console.log(numero1 + numero2 + numero3);
+
+// var textoa = "iron maiden";
+// var textob = "metallica";
+
+// console.log(`yo fui a los recitales ${textoa} ${textob}`);
+// console.log(`yo me voy a los  recitales de ${textoa} y ${textob}`);
+
+// var number = 18;
+// var textoc = "años";
+// var texto1 = "Oviedo";
+// console.log(
+//   `mi nombre es alexander y tengo ${number} ${textoc} y mi apellido es ${texto1}`
+// );
+
+// let nombrelet = "joan";
+// console.log(nombrelet);
+// nombrelet = "oviedo";
+// console.log(nombrelet);
+
+// let abrir = prompt("hola como estas ");
+// console.log(abrir);
+
+// let mebaño = true;
+
+// if (mebaño !== true) {
+//   console.log("me baño dentro de hora");
+// } else {
+//   console.log("no me baño hoy");
+// }
+
+// let hoytoco = true;
+
+// if (hoytoco === true) {
+//   console.log("hoy toco la guitarra alas 7");
+// } else {
+//   console.log("hoy no toco la guitarra estudio");
+// }
+// let suma = 18;
+
+// if (suma > 50) {
+//   console.log("tengo que sumar numeros");
+// } else {
+//   console.log("tengo que restar numeros");
+// }
+
+// let tierra = "fuego";
+
+// if (tierra !== "fuego") {
+//   console.log("tiera y fuego son como el agua y el aceite");
+// } else {
+//   console.log("nada que ver son muy iguales");
+// }
+// let resta = 2500;
+// if (resta < 1) {
+//   console.log("tengo que restar");
+// } else {
+//   console.log("sos un puto");
+// }
+
+// let frutas = "melon";
+
+// switch (frutas) {
+//   case "banana":
+//     console.log("compre banana");
+//     break;
+//   case "naranja":
+//     console.log("compre naranja");
+//     break;
+//   case "melon":
+//     console.log("compre melon");
+//     break;
+//   default:
+//     console.log("no tenia manzanas");
+//     break;
+// }
+// let raza = "salchicha";
+// switch (raza) {
+//   case "pitbull":
+//     console.log("tengo un pitbull");
+//     break;
+//   case "chiguagua":
+//     console.log("tengo un chiguagua");
+//     break;
+//   case "dogo":
+//     console.log("tengo un dogo");
+//     break;
+//   default:
+//     console.log("no tengo animales");
+//     break;
+// }
+// function familiar() {
+// //   console.log("hoy voy a ver a mis familiares");
+// // }
+// // familiar();
+// function mascotas() {
+//   console.log("mi mascota es un perro");
+//   {
+//     if (mascotas === perro) {
+//       console.log("tengo un gato");
+//     }
+//   }
+// }
+// mascotas("gato");
+// // function guitarra(marca) {
+// //   console.log(`voy a comprar ${marca}`);
+// // }
+// guitarra("gibson");
+
+// function colores(celeste) {
+//   console.log(`mi color favorito es ${celeste}`);
+// }
+
+// colores("rojo");
+
+// function telefono(precio, celular) {
+//   console.log(`cuanto es el ${precio} de cada ${celular} negro`);
+// }
+
+// telefono("18", "samsumg");
+
+// function musica(palo, tema) {
+//   console.log(
+//     `cuantos dias pasaran hasta que toque la ${palo} y toque el ${tema} `
+//   );
+// }
+// musica("bateria", "bajo");
+
+// for (let ALEXANDER = 0; ALEXANDER < 20; ALEXANDER++) {
+//   console.log(
+//     `alex hoy se come ala tarde helado una cuantas veces ${ALEXANDER}`
+//   );
+// }
+
+// for (let metodo = 0; metodo < 10; metodo++) {
+//   console.log(`me la paso durmiendo ${metodo}  veces por dia`);
+// }
+
+// for (let judas = 0; judas < 10; judas++) {
+//   console.log(
+//     `yo me la paso escuchando el tema de judas  victimas de cambio ${judas} por dia`
+//   );
+// }
+
+// let oviedo = ["auto", "mecanico", "fierro", "bateria", "moto", "aceite"];
+
+// for (let alex = 0; alex < oviedo.length; alex++) {
+//   console.log(oviedo[alex]);
+// }
+
+// let mascotas = ["pitbull", "dogo", "pekines", "salchicha", "bigils"];
+
+// for (let razas = 0; razas < mascotas.length; razas++) {
+//   console.log(mascotas[razas]);
+// }
+
+// let autos = ["peugeot", "wolsvagen", "fiat", "reanult"];
+
+// for (let joan = 0; joan < autos.length; joan++) {
+//   console.log(autos[joan]);
+// }
+
+// let marcas = ["motorola", "samsumg", "tlc", "sony", "nokia"];
+
+// for (let celular = 0; celular < marcas.length; celular++) {
+//   if (marcas[celular] === "samsug") {
+//     console.log("estoy buscando un samsumg");
+//   }
+// }
+
+// const carne = (nombre, apelllido) => {
+//   console.log(`hola mi nombre es ${nombre} y mi apellido es ${apelllido}`);
+// };
+// carne("alex", "ovi");
+
+// const movistar = (red1, red2) => {
+//   console.log(
+//     `ultimamente no me esta llegando los ${red1} para hacer la coneccion a ${red2}`
+//   );
+// };
+// movistar("cables", "internet");
+
+// const punto = (dedo, feliz) => {
+//   console.log(
+//     `en todas las oracion van los puntos tambien se ponen las ${dedo} y tambien se suele usar ${feliz}. `
+//   );
+// };
+// // punto("comas", "punto y coma");
+
+// let auto = {
+//   constructor(marca, modelo, año, seguro, patente) {
+//     (this.marca = marca),
+//       (this.modelo = modelo),
+//       (this.año = año),
+//       (this.seguro = seguro),
+//       (this.patente = patente);
+//   },
+// };
+
+// let suma = "metegol";
+
+// if (suma === "pelota") {
+//   console.log("voy a meter un golazo");
+// } else {
+//   console.log("fui a errar el gol");
+// }
+// let numero = 200;
+// // if (numero > 500) {
+//   return;
+// // }
+// let persona = {
+//   nombre: "alexander",
+//   apellido: "Oviedo",
+//   sexo: "masculino",
+//   edad: 34,
+//   direccion: "callefalsa 1234",
+// };
+// console.log(persona.nombre);
+// if (persona.nombre !== "alexader") {
+//   console.log("tada!");
+// }
+// let tomate = 1900;
+
+// if (tomate < 2000) {
+//   console.log("el tomate esta 1000 pesos cada uno");
+// } else {
+//   console.log("esta 5000 pesos el kilo");
+// }
+
+// let gaseosas = "pepsi,coca,seven up"
+// 0´'0
+// +
+// // if gaseosa
+// // const saludo = document.getElementById("saludos");
+// // console.log(saludo.innerHTML);
+// // const h2 = document.getElementById("h2");
+// // h2.innerHTML = "hola";
+
+// // console.log(h2.innerText.toUpperCase());
+// // console.log(typeof h2.innerText);
+// // const titulo = document.getElementById("title");
+
+// // // console.dir(titulo);
+// // titulo.innerText = "ALEX";
+
+// // const para = document.getElementById("para");
+// // console.log(para);
+// // para.innerHTML.toUpperCase();
+
+// const tit = document.getElementsByClassName("tit");
+// tit[0].innerText.toUpperCase();
+// console.dir(tit);
+// let obj = {
+//   name: "alex",
+//   lastName: "oviedo",
+// };
+// console.log(obj.name);
+const main = document.getElementById("saludos");
+console.log(main);
+
+const h1 = document.querySelector("h1");
+console.log(h1.innerHTML);
+
+h1.innerHTML = "yo ya no soy un verdadero titulo";
+console.dir(h1);
+h1.innerText = "yo volvi a ser un titulo";
