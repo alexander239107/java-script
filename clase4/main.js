6ec5583c14045e45f3c7b5e22903d34f3aeb686a
alexander <oviedojalexander@gmail.com>
2024-12-04 13:03:01 -0300

    cambio

---
+++ clase4/main.js
@@ -0,0 +1,12 @@
+// JSON
+
+// java scrip objeto notation
+
+let objetomodelo = {
+  nombre: "rodrigo",
+  edad: 32,
+  tecnologias: ["HTML", "CSS", "JAVASCRIP", "REACT"],
+  grabando: true,
+};
+console.log("objeto modelo");
+console.log(objetomodelo);
