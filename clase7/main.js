fc22784eb8754ebc4b88de62b4b3a6d60c9c3989
alexander <oviedojalexander@gmail.com>
2024-12-16 13:09:27 -0300

    cambo

---
+++ clase7/main.js
@@ -0,0 +1,18 @@
+// Math.round
+
+// const round = Math.round(5.6);
+const redondeo = 5.9;
+
+console.log(Math.round(redondeo));
+
+const floor = Math.floor(8.2);
+
+console.log(floor);
+const redo = 7.2;
+console.log(Math.floor(redo));
+
+const cell = 4.2;
+console.log(Math.ceil(cell));
+
+const ceil = 2.5;
+console.log();
